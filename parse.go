29b78136eb7727770b9856190b1e84ae231f5752
Nathan Osman <nathan@quickmediasolutions.com>
2018-06-16 17:03:45 -0700

    Rewrite Parse function to remove tag processing and increase the number of supported types.

--- parse.go
+++ parse.go
@@ -2,60 +2,47 @@ package ezform
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"reflect"
-	"strconv"
-	"strings"
 )
 
 var (
-	// ErrFieldRequired indicates that a required field is missing.
-	ErrFieldRequired = errors.New("field is required")
-
-	// ErrInvalidFieldType indicates that the field type is unsupported.
-	ErrInvalidFieldType = errors.New("invalid field type")
+	errPtrRequired = errors.New("Parse() requires a pointer to struct")
 )
 
-// Parse reads form values from a request, validates them, and stores their
-// value in the provided struct. The return value is a map that lists
-// validation errors by field name.
-func Parse(r *http.Request, v interface{}) (map[string][]error, error) {
+// Parse reads form values for a request, validates them, and stores their
+// value in the provided struct. The first return value maps field names to
+// any errors that occurred during field validation. The second return value
+// represents any global errors that occurred during validation. If both
+// return values are nil, the form was successfully validated.
+func Parse(r *http.Request, v interface{}) (map[string]error, error) {
 	if err := r.ParseForm(); err != nil {
 		return nil, err
 	}
+	vType := reflect.TypeOf(v)
+	if vType.Kind() != reflect.Ptr {
+		return nil, errPtrRequired
+	}
+	vType = vType.Elem()
+	if vType.Kind() != reflect.Struct {
+		return nil, errPtrRequired
+	}
 	var (
-		vType  = reflect.TypeOf(v).Elem()
-		vVal   = reflect.ValueOf(v).Elem()
-		errors = map[string][]error{}
+		vVal        = reflect.ValueOf(v).Elem()
+		fieldErrors = map[string]error{}
 	)
 	for i := 0; i < vType.NumField(); i++ {
 		var (
-			fType = vType.Field(i)
-			fVal  = vVal.Field(i)
-			s     = r.Form.Get(vType.Field(i).Name)
+			fStruct = vType.Field(i)
+			fVal    = vVal.Field(i)
+			fStrVal = r.Form.Get(fStruct.Name)
 		)
-		for _, tag := range strings.Split(fType.Tag.Get("form"), ",") {
-			switch tag {
-			case "required":
-				if len(s) == 0 {
-					errors[fType.Name] = append(
-						errors[fType.Name],
-						ErrFieldRequired,
-					)
-				}
-			}
+		if err := validate(vType, fStruct, vVal, fVal); err != nil {
+			fieldErrors[fStruct.Name] = err
+			continue
 		}
-		switch fVal.Kind() {
-		case reflect.String:
-			fVal.SetString(s)
-		case reflect.Int64:
-			iVal, _ := strconv.ParseInt(s, 10, 64)
-			fVal.SetInt(iVal)
-		case reflect.Bool:
-			fVal.SetBool(len(s) > 0)
-		default:
-			return nil, ErrInvalidFieldType
-		}
+		fmt.Sscanf(fStrVal, "%v", fVal.Addr().Interface())
 	}
-	return errors, nil
+	return fieldErrors, nil
 }
